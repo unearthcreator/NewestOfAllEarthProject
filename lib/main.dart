4b2e48f62f791a49c667ac3d066e78d82c9585c7
unearthcreator <181272960+unearthcreator@users.noreply.github.com>
2024-12-08 10:22:22 +0100

    Sanity test gone in main

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,6 @@ import 'package:flutter/services.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
-import 'package:map_mvp_project/models/annotation.dart';
-import 'package:map_mvp_project/repositories/i_annotations_repository.dart'; // Ensure this import is present
-import 'package:map_mvp_project/repositories/local_annotations_repository.dart';
-
 void main() {
   setupErrorHandling();
   runAppWithErrorHandling(_initializeApp);
@@ -24,35 +20,14 @@ void main() {
 void _initializeApp() async {
   WidgetsFlutterBinding.ensureInitialized();
   logger.i('Initializing app, locking orientation, and initializing Hive.');
-  
+
   await Hive.initFlutter();
-  
+
   await lockOrientation().catchError((error, stackTrace) {
     logger.e('Failed to set orientation', error: error, stackTrace: stackTrace);
   });
 
   _runAppSafely();
-
-  // Quick sanity check after a short delay to ensure the app has started.
-  Future.delayed(const Duration(seconds: 2), () async {
-    // Notice we declare `repo` as `IAnnotationsRepository` instead of `LocalAnnotationsRepository`
-    IAnnotationsRepository repo = LocalAnnotationsRepository();
-    final testAnnotation = Annotation(
-      id: 'test-annotation-id',
-      title: 'Test Annotation',
-      iconName: 'test_icon',
-      date: DateTime.now(),
-      note: 'This is a test note',
-      images: [],
-      latitude: 40.6892,
-      longitude: -74.0445,
-    );
-
-    await repo.addAnnotation(testAnnotation);
-    final annotations = await repo.getAnnotations();
-    // Because Annotation now has a toString(), this will print detailed info.
-    logger.i('Annotations retrieved from local DB: $annotations');
-  });
 }
 
 void _runAppSafely() {
